9f37926fa67150fb3629d78e01584c5555ba3439
Harshil <darjiharshil2994@gmail.com>
2017-01-31 16:20:28 +0530

    avatar upload

--- app/src/main/java/com/example/harshil/charotarexplore/home.java
+++ app/src/main/java/com/example/harshil/charotarexplore/home.java
@@ -1,11 +1,17 @@
 package com.example.harshil.charotarexplore;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v4.view.GravityCompat;
@@ -13,6 +19,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -20,13 +28,28 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.BitmapImageViewTarget;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     cached cached = new cached();
     data data = new data();
-    private AlertDialog exitDialog, outDialog;
+    private static final int CAMERA_REQUEST_CODE = 1;
+    private static final int GALLERY_INTENT = 2;
+    private AlertDialog exitDialog, outDialog, imageMethod;
+    private ProgressDialog uploading;
+    private Bitmap bitmap = null;
     private TextView user_name;
     private ImageView avatar;
     private Button signout;
@@ -91,20 +114,16 @@ public class home extends AppCompatActivity implements NavigationView.OnNavigati
         avatar = (ImageView) headerView.findViewById(R.id.avatar);
 
         user_name.setText(cached.getUser_name(getApplicationContext()));
-        Glide.with(home.this).load(cached.getUser_avatar(getApplicationContext())).asBitmap().centerCrop().error(R.drawable.face).into(new BitmapImageViewTarget(avatar) {
-            @Override
-            protected void setResource(Bitmap resource) {
-                RoundedBitmapDrawable circularBitmapDrawable =
-                        RoundedBitmapDrawableFactory.create(home.this.getResources(), resource);
-                circularBitmapDrawable.setCircular(true);
-                avatar.setImageDrawable(circularBitmapDrawable);
-            }
-        });
+        displayAvatar();
 
+        uploading = new ProgressDialog(home.this);
+        uploading.setCancelable(false);
+        uploading.setTitle("Avatar upload");
+        uploading.setMessage("Avatar upload in progress...");
         avatar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(home.this, "Avatar upload.", Toast.LENGTH_SHORT).show();
+                imageMethod.show();
             }
         });
 
@@ -131,6 +150,40 @@ public class home extends AppCompatActivity implements NavigationView.OnNavigati
                     }
                 }).setCancelable(true);
         exitDialog = exit.create();
+
+        AlertDialog.Builder method = new AlertDialog.Builder(home.this);
+        method.setMessage("Please select method:");
+        method.setPositiveButton("Use Gallery",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        imageMethod.dismiss();
+                        Intent intent = new Intent(Intent.ACTION_PICK);
+                        intent.setType("image/*");
+                        startActivityForResult(intent, GALLERY_INTENT);
+                    }
+                });
+        method.setNegativeButton("Use Camera",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        imageMethod.dismiss();
+                        captureImage();
+                    }
+                });
+        imageMethod = method.create();
+    }
+
+    public void displayAvatar() {
+        Glide.with(home.this).load(decodeStringImage(cached.getUser_avatar(getApplicationContext()))).asBitmap().centerCrop().error(R.drawable.face).into(new BitmapImageViewTarget(avatar) {
+            @Override
+            protected void setResource(Bitmap resource) {
+                RoundedBitmapDrawable circularBitmapDrawable =
+                        RoundedBitmapDrawableFactory.create(home.this.getResources(), resource);
+                circularBitmapDrawable.setCircular(true);
+                avatar.setImageDrawable(circularBitmapDrawable);
+            }
+        });
     }
 
     @Override
@@ -187,5 +240,95 @@ public class home extends AppCompatActivity implements NavigationView.OnNavigati
         TextView village = (TextView) view;
         data.setVid(village.getTag().toString());
         startActivity(new Intent(home.this, category.class));
+    }
+
+    public void captureImage() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{
+                        android.Manifest.permission.CAMERA
+                }, 6);
+            }
+            return;
+        }
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, CAMERA_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 6:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    captureImage();
+                }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uploading.show();
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+            bitmap = (Bitmap) data.getExtras().get("data");
+        } else if (requestCode == GALLERY_INTENT && resultCode == RESULT_OK) {
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (bitmap != null) {
+            toStringImage(bitmap);
+        } else {
+            uploading.dismiss();
+        }
+    }
+
+    public void toStringImage(Bitmap bmp) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        uploadimageapi(encodedImage);
+    }
+
+    public byte[] decodeStringImage(String encodedImage) {
+        byte[] decodedString = Base64.decode(encodedImage, Base64.DEFAULT);
+        //Bitmap decodedImage = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        return decodedString;
+    }
+
+    public void uploadimageapi(String encodedImage) {
+        final String avatar = encodedImage;
+        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = getResources().getString(R.string.link) + "uploadAvatar";
+        StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                uploading.dismiss();
+                cached.setUser_avatar(avatar, getApplicationContext());
+                displayAvatar();
+                Toast.makeText(home.this, "Avatar successfully uploaded.", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("VolleyError", error.toString());
+                uploading.dismiss();
+                Toast.makeText(home.this, "Something is wrong.", Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            protected Map<String, String> getParams() {
+                Map<String, String> MyData = new HashMap<String, String>();
+                MyData.put("user_id", cached.getUser_id(getApplicationContext()));
+                MyData.put("avatar", avatar);
+                return MyData;
+            }
+        };
+        requestQueue.add(MyStringRequest);
     }
 }
